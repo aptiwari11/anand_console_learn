1845f0ca949cbe508b15f083c151c637646224dc
anand.prakash <anand.prakash@epicor.com>
2023-02-06 00:39:42 +0530

    Added new files.

---
+++ ConsoleApp_Learn/Solid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Learn
+{
+    internal class Solid
+    {
+       /* 
+        * S - Single Responsibillity Principal
+        * O - Open Close Principal
+        * L - Linkshow Substitute Principal
+        * I - Interface Segrigation Principal
+        * D - Dependency Inversion Principal
+        */
+
+
+
+    }
+}
